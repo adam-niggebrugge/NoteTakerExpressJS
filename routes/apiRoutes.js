e28fee497f4aea178824324a5d26a35486713e72
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-09-09 11:47:50 -0400

    Removed commented code for homework demo

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 
 router.get('/notes', (req, res) => {
-    res.send("Hello Worlds!!"); //testing
-    // store
-    //     .getNotes()
-    //     .then((notes) => {
-    //         return res.json(notes);
-    //     })
-    //     .catch((err) => res.status(500).json(err));
+    //res.send("Hello Worlds!!"); //testing
+    store
+        .getNotes()
+        .then((notes) => {
+            return res.json(notes);
+        })
+        .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
