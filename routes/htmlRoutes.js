0ce80510f6c98a7008ca7b7331291c57e3ad3d15
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-09-09 15:53:05 -0400

    added generic 404.html to public and route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 
 // GET Route for homepage
 router.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
 // '/notes/ responds with the notes.html file
@@ -12,5 +12,10 @@ router.get('/notes', (req, res) => {
     //find the path dynamically regardles of the remote path set up on server, or locally
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
+
+// GET Route for feedback page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+);
 
 module.exports = router;
