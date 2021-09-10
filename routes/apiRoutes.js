a3c48144144851b0321387f38a8b0b5746a6eaaa
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-09-10 16:03:07 -0400

    Review of homework requirements lead to removal of 404 generic

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,6 +9,24 @@ notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+// DELETE Route for a specific tip
+notes.delete('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        // Make a new array of all tips except the one with the ID provided in the URL
+        const result = json.filter((tip) => tip.note_id !== noteId);
+  
+        // Save that array to the filesystem
+        writeToFile('./db/db.json', result);
+  
+        // Respond to the DELETE request
+        res.json(`Item ${noteId} has been deleted 🗑️`);
+      });
+  });
+  
+
 notes.post('/', (req, res) => {
     console.log(req.body);
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,18 @@
 const path = require('path');
 const router = require('express').Router();
 
-// GET Route for homepage
-router.get('/', (req, res) =>
+// '/notes/ responds with the notes.html file
+router.get('/notes', (req, res) => {
+  //find the path dynamically regardles of the remote path set up on server, or locally
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+// GET homepage for any routes specified. Order matters, wildcard must be last or it will take precedent.
+router.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
-// '/notes/ responds with the notes.html file
-router.get('/notes', (req, res) => {
-    //find the path dynamically regardles of the remote path set up on server, or locally
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-});
 
-// Wildcard route to direct users to a 404 page
-router.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/404.html'))
-);
+
 
 module.exports = router;
